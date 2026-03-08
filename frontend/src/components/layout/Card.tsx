e7b0058a0f3a36c8e11963ba95db940985008a8b
Agstgrss <augustograssi0101@gmail.com>
2026-03-08 12:26:46 -0300

    Fix: card

--- frontend/src/components/layout/Card.tsx
+++ frontend/src/components/layout/Card.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 interface CardProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   title?: string;
   subtitle?: string;
   footer?: React.ReactNode;
