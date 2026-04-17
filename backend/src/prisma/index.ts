7764ea828bb77786a703a12490249b93127ab031
Agstgrss <augustograssi0101@gmail.com>
2026-04-17 14:25:55 -0300

    great adjust

--- backend/src/prisma/index.ts
+++ backend/src/prisma/index.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from "../generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${process.env.DATABASE_URL}`;
+const connectionString = process.env.DATABASE_URL!;
 const adapter = new PrismaPg({ connectionString });
 
-const prismaClient = new PrismaClient({ adapter });
+const prismaClient = new PrismaClient({
+  adapter,
+});
 
 export default prismaClient;
